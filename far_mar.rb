fc33d5eab99febdd412d7c97130cf101098e2f50
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2016-09-05 18:56:44 -0700

    create far_mar.rb

---
+++ far_mar.rb
@@ -0,0 +1,10 @@
+
+# gems your project needs
+require 'csv'
+
+# our namespace module
+module FarMar; end
+
+# all of our data classes that live in the module
+require 'lib/farmar_market'
+# ...require all needed classes
